d31bf3383ce19bf8f04bf280a615745c6ba78478
Lakshan Sandanayaka <81616253+Lakshan099@users.noreply.github.com>
2021-04-04 02:00:02 +0530

    Add files via upload

---
+++ mc.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <sys/mman.h>
+#include <string.h>
+
+char code[] = {0x55,0x48,0x89,0xe5,0x89,0x7d,0xfc,0x48,
+    0x89,0x75,0xf0,0xb8,0x2a,0x00,0x00,0x00,0xc9,0xc3,0x00};
+
+00000000004004b4 <main> 55                          push   %rbp
+00000000004004b5 <main+0x1>  48 89 e5               mov    %rsp,%rbp
+00000000004004b8 <main+0x4>  89 7d fc               mov    %edi,-0x4(%rbp)
+00000000004004bb <main+0x7>  48 89 75 f0            mov    %rsi,-0x10(%rbp)
+return 42;
+00000000004004bf <main+0xb>  b8 2a 00 00 00         mov    $0x2a,%eax
+00000000004004c4 <main+0x10> c9                     leaveq 
+00000000004004c5 <main+0x11> c3                     retq 
+
+
+int main(int argc, char **argv) { 
+    void *buf;
+
+    
+    buf = mmap (0,sizeof(code),PROT_READ|PROT_WRITE|PROT_EXEC,
+                MAP_PRIVATE|MAP_ANON,-1,0);
+    memcpy (buf, code, sizeof(code));
+    __builtin___clear_cache(buf, buf+sizeof(code)-1); 
+
+   
+    int i = ((int (*) (void))buf)();
+    printf("return value: %d", i);
+
+    return 0;
+}
